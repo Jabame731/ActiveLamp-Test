5abed086b48ef8bbb758eaa13f4a2114e510a3d5
Jabame731 <94708537+Jabame731@users.noreply.github.com>
2023-04-28 23:15:26 +0800

    Delete test-activelamp-frontend/src/app/url/types directory

--- test-activelamp-frontend/src/app/url/types/urlState.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UrlInterface } from './url.interface';
-
-export interface UrlStateInterface {
-  isLoading: boolean;
-  urls: UrlInterface[];
-  selectedUrl: string;
-  isError: string | null;
-}
